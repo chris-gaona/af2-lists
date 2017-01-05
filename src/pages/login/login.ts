df7c09b2d7511ed91921741e1d43957f076e3e0e
Chris Gaona <thebeloved88@sbcglobal.net>
2017-01-05 15:54:53 -0800

    Creates the login page and adds email validator

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,22 +1,84 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { AuthData } from '../../providers/auth-data';
 
-/*
-  Generated class for the Login page.
+import { EmailValidator } from '../../validators/email';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import { HomePage } from '../home/home';
+import { SignupPage } from '../signup/signup';
+import { ResetPasswordPage } from '../reset-password/reset-password';
+
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  loginForm: any;
+  emailChanged: boolean = false;
+  passwordChanged: boolean = false;
+  submitAttempt: boolean = false;
+  loading: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authData: AuthData,
+              public formBuilder: FormBuilder, public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController) {
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required,
+        EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6),
+        Validators.required])]
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
+
+  goToResetPassword(){
+    this.navCtrl.push(ResetPasswordPage);
+  }
+
+  createAccount(){
+    this.navCtrl.push(SignupPage);
+  }
+
+  elementChanged(input){
+    let field = input.inputControl.name;
+    this[field + "Changed"] = true;
+  }
+
+  loginUser(){
+    this.submitAttempt = true;
+
+    if (!this.loginForm.valid){
+      console.log(this.loginForm.value);
+    } else {
+      this.authData.loginUser(this.loginForm.value.email,
+        this.loginForm.value.password).then( authData => {
+        this.navCtrl.setRoot(HomePage);
+      }, error => {
+        this.loading.dismiss().then( () => {
+          let alert = this.alertCtrl.create({
+            message: error.message,
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+            ]
+          });
+          alert.present();
+        });
+      });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
+  }
+
 
 }
