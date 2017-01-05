3c8fa0772d511cac0667254030313077ec0c4116
Chris Gaona <thebeloved88@sbcglobal.net>
2017-01-05 14:38:28 -0800

    Creates authentication observer to direct user to login or home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,26 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+
+import { AngularFire } from 'angularfire2';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage: any;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, public af: AngularFire) {
+    af.auth.subscribe( user => {
+      if (user) {
+        this.rootPage = HomePage;
+      } else {
+        this.rootPage = LoginPage;
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
